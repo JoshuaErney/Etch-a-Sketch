7a42bf15e31b90e57de5e02b56661bd72f7abc66
Joshua Erney <57075505+JoshuaErney@users.noreply.github.com>
2024-07-30 23:45:50 -0400

    updated js file to include color select

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
-const form = document.querySelector("#squareForm");
+const squareForm = document.querySelector("#squareForm");
+const colorForm = document.querySelector("#colorForm");
+
 const input = document.querySelector("#numOfSquares");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent the default form submission behavior
-  const value = input.value;
-  console.log(value); // Now this will correctly log the updated value
+const btnUpdateSquares = document.querySelector("#update-squares-btn");
+const btnColorSelect = document.querySelector("#color-select-btn");
+
+const dialogUpdate = document.querySelector("#dialogUpdate");
+const dialogColor = document.querySelector("#dialogColor");
+
+const btnFormCancel = document.querySelector("#cancelBtn");
+
+squareForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  squareForm.querySelector(`input[type="number"]`);
+  const numValue = input.value;
+  dialogUpdate.close();
+  return numValue;
 });
+
+colorForm.addEventListener("submit", (e) => {
+  const selectElement = colorForm.querySelector("select");
+  const colorValue = selectElement.value;
+  dialogUpdate.close();
+  return colorValue;
+});
+
+// Opens the Update dialog
+btnUpdateSquares.addEventListener("click", () => {
+  dialogUpdate.show();
+});
+
+// Opens the Color dialog
+btnColorSelect.addEventListener("click", () => {
+  dialogColor.show();
+});
+
+// Closes the Update dialog (via the cancel button)
+btnFormCancel.addEventListener("click", () => {
+  dialogUpdate.close();
+});
+
+function createGrid(params) {
+  for (let i = 0; i < 256; i++) {}
+}
